62ec0d15bdadccc073171665622812722bfb4d77
Ara Grigsby <74517531+aganail9@users.noreply.github.com>
2021-01-07 14:23:31 -0600

    Add files via upload

---
+++ new project/src/com/company/Main.java
@@ -0,0 +1,52 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	  // System.out.println("Hello Java");
+	// System.out.println("hi");
+	// System.out.print("hey")
+        Scanner input = new Scanner(System.in);
+    //    System.out.println("what is your name");
+    //    String firstName = input.nextLine();
+     //   System.out.printf("Hello %s!\n", firstName);
+        //   System.out.println("What is your last name?");
+       // String lastName = input.nextLine();
+     //   System.out.printf("your name is %s %s. \n", firstName, lastName);
+     //   System.out.println("Enter a name: ");
+     //   String name = input.nextLine();
+     //   System.out.println("Give me an adverb: ");
+      //  String adverb = input.nextLine();
+      //  System.out.println("Give me an adjective: ");
+     //   String adjective = input.nextLine();
+     //   System.out.println("Give me an verb: ");
+     //   String verb = input.nextLine();
+     //   System.out.println("Give me a noun two: ");
+     //   String nounTwo = input.nextLine();
+    //    System.out.println("Give me a number: ");
+     //   String number = input.nextLine();
+     //   System.out.println("Give me your name: ");
+     //   String yourName = input.nextLine();
+
+    //    System.out.printf("Here is your story: \n");
+     //   System.out.printf(" \n Dear %s,", name);
+     //   System.out.printf(" \n You are %s %s and I want to be your %s!", adverb, adjective, verb);
+       // System.out.printf(" \n I want to go to the %s with you in %s days.", nounTwo, number);
+        //System.out.printf(" \n Sincerely, %s \n", yourName);
+
+byte b = (byte)1234;
+
+double num = 5;
+double num2 = 10;
+
+7777
+boolean result = num % num2 == 0;
+
+    System.out.println(result);
+
+
+
+    }
+}
